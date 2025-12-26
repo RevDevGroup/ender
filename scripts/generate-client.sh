0862c244fffdde9a229d70feb4e804770b8e8ddf
Jimmy Angel Pérez Díaz <jianpedi@gmail.com>
2025-12-26 15:25:00 -0500

    Initial commit

---
+++ scripts/generate-client.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+set -e
+set -x
+
+cd backend
+python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
+cd ..
+mv openapi.json frontend/
+cd frontend
+npm run generate-client
